297a8099ebbba6772f1fbad4c9f7510d3a64deae
inornnut01 <inornnut01@gmail.com>
2025-05-18 11:50:34 +0700

    protectRoute js

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import cookieParser from "cookie-parser";
 
 import { ENV_VATS } from "./config/envVars.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 //import searchRoutes from "./routes/search.route.js";
+
 import connectMongoDB from "./config/db.js";
 
 const app = express();
@@ -16,8 +19,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 //app.use("/api/v1/search", searchRoutes);
 
 app.listen(PORT, () => {
