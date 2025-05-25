3f63c632a0ae60b1d4fa758f98f8b9162486d051
inornnut01 <inornnut01@gmail.com>
2025-05-25 16:05:09 +0700

    Building the Search History Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from './pages/LoginPage'
 import Footer from './components/Footer'
 import WatchPage from './pages/WatchPage'
 import SearchPage from './pages/SearchPage'
+import HistoryPage from './pages/HistoryPage'
 import { Toaster } from 'react-hot-toast'
 import { useAuthStore } from './store/authUser'
 import { useEffect } from 'react'
@@ -35,6 +36,7 @@ function App() {
         <Route path="/login" element={!user ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/watch/:id" element={user ? <WatchPage /> : <Navigate to="/login" />} />
         <Route path= "/search" element={user ? <SearchPage /> : <Navigate to="/login" />}/>
+        <Route path="/history" element={user ? <HistoryPage /> : <Navigate to="/login" />}/>
       </Routes>
       <Footer />
       <Toaster />
